5985c841151da5df5e3dafd325c99a5ee0bcfe7c
Matt Kelly <bmatthewkelly@gmail.com>
2018-07-24 19:40:14 +0000

    refactored lookup_people.js, separated query into functions

--- lookup_people.js
+++ lookup_people.js
@@ -10,25 +10,43 @@ const client = new pg.Client({
   ssl: settings.ssl
 });
 
+// command line arguments
 const arg = process.argv[2];
-const values = [arg];
 
+// connect to database
 client.connect((err) => {
   if (err) {
     console.error("Connection Error", err);
     return false;
   }
-  client.query("SELECT first_name, last_name, TO_CHAR(birthdate, 'yyyy-mm-dd') AS birthdate, ROW_NUMBER () OVER (ORDER BY id) FROM famous_people WHERE first_name LIKE $1::text", values, (err, result) => {
-    if (err) {
-      return console.error("error running query", err);
-    }
-    console.log("Searching ...")
-    console.log(`Found ${result.rowCount} person(s) by the name '${values[0]}':`)
-    result.rows.forEach((person) => {
-      console.log(`- ${person.row_number}: ${person.first_name} ${person.last_name}, born ${person.birthdate}`)
-    });
-    client.end();
-  });
 });
 
-function dbQuery() {};
+// database access function
+function runQuery(query, values, cb) {
+  client.query(query, values, (err, res) => {
+    if (err) {
+      console.error(err.stack);
+      return false;
+    }
+    // run callback on res.rows
+    cb(res.rows);
+    client.end();
+  });
+}
+
+// search database by first_name
+function queryByFirstName(name) {
+  const query = "SELECT first_name, last_name, TO_CHAR(birthdate, 'yyyy-mm-dd') AS birthdate, ROW_NUMBER () OVER (ORDER BY id) FROM famous_people WHERE first_name LIKE $1::text";
+  const values = [name];
+
+  runQuery(query, values, (data) => {
+    console.log("Searching ...");
+    console.log(`Found ${data.length} person(s) by the name '${values[0]}':`);
+    data.forEach((person) => {
+      console.log(`- ${person.row_number}: ${person.first_name} ${person.last_name}, born ${person.birthdate}`);
+    });
+  })
+};
+
+// call first_name function
+queryByFirstName(arg);
